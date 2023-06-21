d2b2e9bf127c70efd5363778139073d4de9d0b35
Crowdbotics <engineering@crowdbotics.com>
2023-06-21 23:21:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "silent-disk-dev-93650",
+  url: "https://silent-disk-dev-93650.botics.co",
+  api: "https://silent-disk-dev-93650.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
